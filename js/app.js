9d5b1c96eefdb5411934a1331015945f9649bf4f
mcchan1 <mcchan1@gmail.com>
2015-11-16 13:32:32 -0500

    one google map info window working

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var locations = [
 		"lat": '40.773609', 
 		"lng": '-73.989434',
 		"position":'Partner',
-		"description":'Legal eagle'
+		"description":'Legal eagle dubbabeee fm'
 	},
 ];
 function initMap() {
@@ -19,12 +19,43 @@ function initMap() {
     center: NewYorkCity,
     zoom: 16
   });
+  
+  	var locationsString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      'locations.description'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  	var infowindow = new google.maps.InfoWindow({
+  		content: locationsString
+  	});
+
   	var locations = new google.maps.Marker({
   		position: NewYorkCity,
+  		map: map,
   		title: "Qoo"
   	});
-  	locations.setMap(map);
+
+  	locations.addListener('click',function(){
+  		infowindow.open(map, locations);
+  	});
+
+
+  	//sets location marker
+  	// locations.setMap(map);
+
 };//initMap
+
 // Here's my data model
 var ViewModel = function(first, last) {
     this.firstName = ko.observable(first);
