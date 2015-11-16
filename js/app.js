9d1fe69eeed3ff47e513f9e2dbc48e48e4f2b428
mcchan1 <mcchan1@gmail.com>
2015-11-16 13:00:43 -0500

    google map marker works

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 
-var self = this;
-var map, NewYorkCity, infowindow;
 
+// var self = this;
+// var map, NewYorkCity, infowindow;
+var locations = [
+	{	
+		"title": 'Melwani & Chan LLP',
+		"date": '2012 to Present',
+		"lat": '40.773609', 
+		"lng": '-73.989434',
+		"position":'Partner',
+		"description":'Legal eagle'
+	},
+];
 function initMap() {
+ 
 	NewYorkCity = new google.maps.LatLng(40.7642846,-73.9818741);
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
+  	map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: NewYorkCity,
     zoom: 16
   });
-};
-
+  	var locations = new google.maps.Marker({
+  		position: NewYorkCity,
+  		title: "Qoo"
+  	});
+  	locations.setMap(map);
+};//initMap
 // Here's my data model
 var ViewModel = function(first, last) {
     this.firstName = ko.observable(first);
@@ -21,4 +36,4 @@ var ViewModel = function(first, last) {
     }, this);
 };
  
-ko.applyBindings(new ViewModel("Planet", "Earth")); 
+ko.applyBindings(new ViewModel("Qoo", "Earth")); 
