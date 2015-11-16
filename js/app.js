4e83660ca7e232361a60e9ffb6334805e4f46dc5
mcchan1 <mcchan1@gmail.com>
2015-11-16 11:31:30 -0500

    test of map api and ko api successful

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 
-var map;
+var self = this;
+var map, NewYorkCity, infowindow;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 34.397, lng: -75.644},
-    zoom: 8
+	NewYorkCity = new google.maps.LatLng(40.7642846,-73.9818741);
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: NewYorkCity,
+    zoom: 16
   });
-}
+};
+
+// Here's my data model
+var ViewModel = function(first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+ 
+    this.fullName = ko.pureComputed(function() {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+ 
+ko.applyBindings(new ViewModel("Planet", "Earth")); 
