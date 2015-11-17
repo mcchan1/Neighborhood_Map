0d304285063e6ccbf37ffd63c4a60ad57bd13878
mcchan1 <mcchan1@gmail.com>
2015-11-17 14:05:47 -0500

    add search bar and css
    added search bar with google places.
    made separate css file

--- js/app.js
+++ js/app.js
@@ -1,60 +1,92 @@
 
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
 
-// var self = this;
-// var map, NewYorkCity, infowindow;
-var locations = [
-	{	
-		"title": 'Melwani & Chan LLP',
-		"date": '2012 to Present',
-		"lat": '40.773609', 
-		"lng": '-73.989434',
-		"position":'Partner',
-		"description":'Legal eagle dubbabeee fm'
-	},
-];
-function initMap() {
- 
-	NewYorkCity = new google.maps.LatLng(40.7642846,-73.9818741);
-  	map = new google.maps.Map(document.getElementById('map-canvas'), {
+function initAutocomplete() {
+   NewYorkCity = new google.maps.LatLng(40.7642846,-73.9818741);
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: NewYorkCity,
-    zoom: 16
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+//INFO WINDOW
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+
+  service.getDetails({
+    placeId: 'ChIJe_T71flYwokRGGZTwkP_vL8'
+  }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    }
   });
   
-  	var locationsString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      'locations.description'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  	var infowindow = new google.maps.InfoWindow({
-  		content: locationsString
-  	});
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+  
 
-  	var locations = new google.maps.Marker({
-  		position: NewYorkCity,
-  		map: map,
-  		title: "Qoo"
-  	});
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
 
-  	locations.addListener('click',function(){
-  		infowindow.open(map, locations);
-  	});
+    if (places.length == 0) {
+      return;
+    }
 
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-  	//sets location marker
-  	// locations.setMap(map);
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-};//initMap
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
+}
+
 
 // Here's my data model
 var ViewModel = function(first, last) {
