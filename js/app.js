612d2b976c749b5e6f33a95d2bdf2db21d9e6155
mcchan1 <mcchan1@gmail.com>
2015-11-18 14:14:24 -0500

    add data-bind to input
    still need to wrap map inside knockout bindings…sigh

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
 
-function initAutocomplete() {
+
+function initMap() {
    NewYorkCity = new google.maps.LatLng(40.7642846,-73.9818741);
   var map = new google.maps.Map(document.getElementById('map'), {
     center: NewYorkCity,
@@ -84,19 +85,12 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
-  // [END region_getplaces]
-}
-
-
-// Here's my data model
-var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
- 
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+  
 };
- 
-ko.applyBindings(new ViewModel("Qoo", "Earth")); 
+
+function ViewModel() {
+  var self = this;
+   self.inputValue = ko.observable("New York City")
+ };
+ ko.applyBindings(new ViewModel());
+
